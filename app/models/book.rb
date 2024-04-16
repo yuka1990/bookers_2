75fea18b21978f22c96dfd8764a1322c697d881b
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-16 23:21:43 +0000

    [update]0417first

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,7 @@ class Book < ApplicationRecord
    belongs_to :user
    
    validates :title, presence: true
-   validates :body, presence:true
-　 validates :body, length: {maximum:200}
+   validates :body, presence:true, length: {maximum: 200}
    
    
    
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
 has_many :books, dependent: :destroy
 has_one_attached :profile_image
 
-validates :name, presence: true
-validates :name, length: {minimum:2,maximum:20}
+validates :name, presence: true, length: {minimum:2,maximum:20}
 validates :introduction, length: {maximum:50}
 
 
