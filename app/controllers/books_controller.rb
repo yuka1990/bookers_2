ddf6d803a41ff9c380569a1f0952d91385d08f4e
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-18 01:14:51 +0000

    [update]0418first

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,7 +28,7 @@ class BooksController < ApplicationController
 
   def show
   @book = Book.find(params[:id])
-  @user = User.find(params[:id])
+  @user = @book.user
   @books = @user.books
   end
   
