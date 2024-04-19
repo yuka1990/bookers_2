6b11b19f6eb6af61a369455a3fb1727e63aa096b
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-19 03:49:29 +0000

    [update]0419revise1

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
  resources :books, only: [:new, :create,:index, :show,:destroy,:edit,:update]
  resources :users, only:[:show, :edit,:update,:index,:create]
-get 'homes/about' => 'homes#about', as: "about"
+get 'home/about' => 'homes#about', as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
