3852c0234e2d68753762d129e27e02d2f43dbc73
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-19 06:40:46 +0000

    [update]0419revise3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -55,8 +55,8 @@ class BooksController < ApplicationController
   end
   
   def is_matching_login_user
-    user = User.find(params[:id])
-    unless user.id == current_user.id
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
       redirect_to books_path
     end
   end
